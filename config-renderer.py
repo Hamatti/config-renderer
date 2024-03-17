c668a3dc2602cd3a6c5c83b00cbfe95c7fc77b0f
Juha-Matti Santala <juhamattisantala@gmail.com>
2024-03-17 17:19:03 +0200

    Initial commit

---
+++ config-renderer.py
@@ -0,0 +1,203 @@
+"""Configuration renderer
+
+Renders a configuration file into an HTML page based on custom minimal markup language.
+
+Supports any config file where comments start with #.
+
+Usage:
+    config-renderer.py <configfile> [-s <stylesheet> | -b] [-o <outfile>] [--class-prefix <prefix>]
+
+Options:
+    -s --style <file>  Filename of a stylesheet to be added
+    -b --body-only  Don't add html, head and body elements. Meant for embedding to existing sites.
+    -o --output <file>  Filename for where to write the file [default: config.html]
+    --class-prefix <prefix>  Customize CSS class prefix [default: configuration]
+
+Markup:
+    - First line should start with "# HEADING" where heading gets added to <h1> and <title>
+    - # [Heading 2] -> <h2>Heading 2</h2>
+    - #> class -> <div class="class">
+    - #< <- </div>
+    - #~ Any line starting with #~ is ignored by the parser.
+    - # Any line starting with a # that does not match any of above, will be added into a <p>paragraph</p>. An empty line, a config line or any of the above will wrap up the paragraph.
+"""
+
+import re
+import sys
+from docopt import docopt
+
+# Match a "# [Heading]" line and capture the text inside the square brackets
+HEADING_PATTERN = r"^# ?\[(.*)\]$"
+
+# Match URLs that are not prepended by "(" (to avoid double rendering Markdown links)
+URL_PATTERN = r"(?<!\()https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+
+# Match Markdown style URL pattern ([Title](url))
+MARKDOWN_URL_PATTERN = r"\[(.*)\]\((.*)\)"
+
+LINE_BREAK = "\n"
+EMPTY_LINE = ""
+
+
+def render_isolated_link(match, class_prefix):
+    """re.sub callable to replace an isolated link with an HTML anchor tag"""
+    url = match.group()
+    return f'<a class="{class_prefix}-link" href="{url}">{url}</a>'
+
+
+def render_markdown_link(match, class_prefix):
+    """re.sub callable to replace Markdown style link [title](url) with an HTML anchor tag"""
+    title, url = match.groups()
+    return f'<a class="{class_prefix}-link" href="{url}">{title}</a>'
+
+
+def render_config_file(filename, class_prefix):
+    """Renders HTML markup for a configuration file provided as argument"""
+    try:
+        output = []
+        with open(filename, "r") as config:
+            in_code_block = False
+            in_paragraph_block = False
+
+            title = next(config)[2:].strip()
+            output.append(f'<h1 class="{class_prefix}-heading">{title}</h1>')
+
+            for line in config:
+                if (
+                    line == LINE_BREAK or line == EMPTY_LINE
+                ):  # Empty line, end tags if needed
+                    if in_code_block:
+                        output.append("</code></pre>")
+                        in_code_block = False
+                    elif in_paragraph_block:
+                        output.append("</p>")
+                        in_code_block = False
+                elif line.startswith("#"):  # Markup
+                    # End any code tags if open
+                    if in_code_block:
+                        output.append("</code></pre>")
+                        in_code_block = False
+
+                    # Match custom markup
+
+                    # Ignore lines that start #~
+                    if line.startswith("#~"):
+                        continue
+
+                    # Heading level 2
+                    elif match := re.match(HEADING_PATTERN, line):
+                        if in_paragraph_block:
+                            output.append("</p>")
+                            in_paragraph_block = False
+                        output.append(
+                            f'<h2 class="{class_prefix}-heading">{match.groups()[0]}</h2>'
+                        )
+                        in_paragraph_block = False
+
+                    # <hr> divider
+                    elif line.startswith("#=") or line.startswith("# ="):
+                        if in_paragraph_block:
+                            output.append("</p>")
+                            in_paragraph_block = False
+                        output.append('<hr class="{class_prefix}-divider" />')
+
+                    # Start <div> tag
+                    elif line.startswith("#> "):
+                        if in_paragraph_block:
+                            output.append("</p>")
+                            in_paragraph_block = False
+                        output.append(
+                            f'<div class="{class_prefix}-container {line[3:]}">'
+                        )
+
+                    # End </div> tag
+                    elif line.startswith("#<"):
+                        if in_paragraph_block:
+                            output.append("</p>")
+                            in_paragraph_block = False
+                        output.append("</div>")
+
+                    # Add text to paragraph tags
+                    else:
+                        if not in_paragraph_block:
+                            output.append('<p class="container-paragraph">')
+                        line = line[1:].strip()
+                        output.append(line)
+                        in_paragraph_block = True
+
+                # Code lines in configuration
+                else:
+                    if in_paragraph_block:
+                        output.append("</p>")
+                        in_paragraph_block = False
+                    if in_code_block:
+                        output.append(f"<span>{line.strip()}</span>")
+                    else:
+                        in_code_block = True
+                        output.append(
+                            f'<pre class="{class_prefix}-pre"><code><span>{line.strip()}</span>'
+                        )
+
+            # Close any code or paragraph tags that are still open
+            if in_code_block:
+                output.append("</code></pre>")
+            elif in_paragraph_block:
+                output.append("</p>")
+
+            html = "\n".join(output)
+
+            # Change URLs to anchor tags
+
+            ## Using these two wrappers to pass class_prefix to the callable
+            def isolated_callable_wrapper(match):
+                return render_isolated_link(match, class_prefix)
+
+            def markdown_callable_wrapper(match):
+                return render_markdown_link(match, class_prefix)
+
+            html = re.sub(URL_PATTERN, isolated_callable_wrapper, html)
+            html = re.sub(MARKDOWN_URL_PATTERN, markdown_callable_wrapper, html)
+
+            return title, html
+
+    except FileNotFoundError:
+        print(f"Error: could not open file '{filename}'")
+        sys.exit(1)
+
+
+def main():
+    # Initialize arguments based on docstring
+    arguments = docopt(__doc__, version="config-renderer 1.0")
+
+    # Render HTML
+    title, body = render_config_file(
+        arguments["<configfile>"], class_prefix=arguments.get("--class-prefix")
+    )
+
+    # Read styles if passed with --style (or -s)
+    styles = ""
+    if stylesheet := arguments.get("--style"):
+        try:
+            with open(stylesheet, "r") as stylefile:
+                styles = stylefile.read()
+        except FileNotFoundError:
+            print(f"Error: stylesheet file '{stylesheet}' not found")
+            sys.exit(1)
+
+    # If --body-only is provided, don't add styles and don't wrap body into html and body tags.
+    if arguments.get("--body-only", False):
+        html = body
+    else:
+        html = f"<!doctype html><html><head><title>{title}</title><style>{styles}</style></head><body><main>{body}</main></body></html>"
+
+    # Get output filename
+    output_filename = arguments.get("--output", "config.html")
+
+    # Write HTML to the output file
+    with open(output_filename, "w") as outfile:
+        outfile.write(html)
+        print(f"Successfully wrote the config html page to {output_filename}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,121 @@
+# config-renderer
+
+config-renderer is a Python CLI utility that reads a configuration file and optional CSS stylesheet and renders a nice looking HTML page from it.
+
+See example documentation page in https://hamatti.github.io/config-renderer/
+
+## Motivation
+
+I like to document my configuration files well so I can understand and remember them well. I also like sharing my configurations in the web and I wanted a way to write configuration files that are human-readable and computer-understandable. With this renderer, I can run a single command to generate an HTML page from the source file so I don't need to maintain two separate documentations.
+
+## Installation
+
+Clone the project:
+
+```
+git clone git@github.com:Hamatti/config-renderer.git
+```
+
+## Usage
+
+```
+Configuration renderer
+
+Renders a configuration file into an HTML page based on custom minimal markup language.
+
+Supports any config file where comments start with #.
+
+Usage:
+    config-renderer.py <configfile> [-s <stylesheet> | -b] [-o <outfile>] [--class-prefix <prefix>]
+
+Options:
+    -s --style <file>  Filename of a stylesheet to be added
+    -b --body-only  Don't add html, head and body elements. Meant for embedding to existing sites.
+    -o --output <file>  Filename for where to write the file [default: config.html]
+    --class-prefix <prefix>  Customize CSS class prefix [default: configuration]
+
+Markup:
+    - First line should start with "# HEADING" where heading gets added to <h1> and <title>
+    - # [Heading 2] -> <h2>Heading 2</h2>
+    - #> class -> <div class="class">
+    - #< <- </div>
+    - #~ Any line starting with #~ is ignored by the parser.
+    - # Any line starting with a # that does not match any of above, will be added into a <p>paragraph</p>. An empty line, a config line or any of the above will wrap up the paragraph.
+```
+
+## Custom markup
+
+First line needs to start with `# TITLE` that is turned into `<h1>TITLE</h1>` and `<title>TITLE</title>`
+
+| Markup                         | Output                                                  | Notes                                                              |
+| ------------------------------ | ------------------------------------------------------- | ------------------------------------------------------------------ |
+| `# [Heading 2]`                | `<h2>Heading 2</h2>`                                    |                                                                    |
+| `#> class`                     | `<div class="class>`                                    |                                                                    |
+| `#<`                           | `</div>`                                                |                                                                    |
+| `#~`                           | (nothing, lines are ignored)                            |                                                                    |
+| `# anything else`              | `<p>anything else</p>`                                  | Multiple consecutive lines with # are combined into same paragraph |
+| `any line not starting with #` | `<pre><code>any line not starting with #</pre></code>`  | Multiple consecutive lines are combined into same code block       |
+| `[Website title](website url)` | `<a href="website url">Website title</a>`               |                                                                    |
+| `https://example.com`          | `<a href="https://example.com">https://example.com</a>` |                                                                    |
+
+## Custom styling
+
+You can provide your own CSS file with `--style FILENAME` option. The styles get added to the `<head>` of the document so the end result is self-standing package.
+
+The tags are marked with following classes:
+
+| Tag        | Class                   |
+| ---------- | ----------------------- |
+| `h1`, `h2` | configuration-heading   |
+| `a`        | configuration-link      |
+| `pre`      | configuration-pre       |
+| `p`        | configuration-paragraph |
+| `div`      | configuration-container |
+
+You can customize these with the `--class-prefix PREFIX` option where `PREFIX` replaces `configuration` in above examples.
+
+## Example
+
+File called `example.conf`
+
+```
+#+ Juhis' tmux configuration
+
+# [Install plugins]
+#~ A list of the plugins
+
+# [Tmux Plugin Manager](https://github.com/tmux-plugins/tpm) to install other plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-resurrect'
+```
+
+ran with
+
+```
+python config-renderer.py example.conf
+```
+
+results in
+
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Juhis' tmux configuration</title>
+    <style></style>
+  </head>
+  <body>
+    <main>
+      <h1>Juhis' tmux configuration</h1>
+      <h2>Install plugins</h2>
+      <p>
+        <a href="https://github.com/tmux-plugins/tpm">Tmux Plugin Manager</a> to
+        install other plugins
+      </p>
+      <pre><code><span>set -g @plugin 'tmux-plugins/tpm'</span>
+<span>set -g @plugin 'tmux-plugins/tmux-resurrect'</span>
+</code></pre>
+    </main>
+  </body>
+</html>
+```
